3a7e6a00dceb7c671b8c656af2ccadda4825ff4b
CliffCrerar <cliff.crerar@gmail.com>
2020-08-08 21:03:26 +0200

    rebuild

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import 'skeleton-css/css/normalize.css'
 import 'skeleton-css/css/skeleton.css'
 import './style.css'
 import 'script-loader!animejs/lib/anime.min.js'; 
-import './script'
+import 'script-loader!./script'
